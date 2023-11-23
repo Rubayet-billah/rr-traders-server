1f3eb2b50adb10c9c2640ab40e9e778992328aaa
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-24 01:41:18 +0600

    logger stopped

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -4,8 +4,6 @@ import { createLogger, format, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 const { combine, timestamp, label, printf } = format;
 
-//Customm Log Format
-
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp);
   const hour = date.getHours();
@@ -56,4 +54,4 @@ const errorlogger = createLogger({
   ],
 });
 
-export { logger, errorlogger };
+export { errorlogger, logger };
