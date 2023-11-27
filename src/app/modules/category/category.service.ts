d7e314a5d64992abc117f951e22cb8183a7e9238
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-27 20:23:01 +0600

    category create successfull

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -5,69 +5,49 @@ import ApiError from '../../../errors/ApiError';
 const prisma = new PrismaClient();
 
 const createCategory = async (categoryData: Category) => {
-  try {
-    const result = await prisma.category.create({
-      data: categoryData,
-    });
-    return result;
-  } catch (error) {
-    throw new Error('Failed to create category'); // Handle error as needed
-  }
+  const result = await prisma.category.create({
+    data: categoryData,
+  });
+  return result;
 };
 
 const getAllCategories = async () => {
-  try {
-    const categories = await prisma.category.findMany();
-    return categories;
-  } catch (error) {
-    throw new Error('Failed to fetch categories'); // Handle error as needed
-  }
+  const categories = await prisma.category.findMany();
+  return categories;
 };
 
 const getCategoryById = async (categoryId: number) => {
-  try {
-    const category = await prisma.category.findUnique({
-      where: {
-        id: categoryId,
-      },
-    });
-    if (!category) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
-    }
-    return category;
-  } catch (error) {
-    throw new Error('Failed to fetch category by ID'); // Handle error as needed
+  const category = await prisma.category.findUnique({
+    where: {
+      id: categoryId,
+    },
+  });
+  if (!category) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
   }
+  return category;
 };
 
 const updateCategory = async (
   categoryId: number,
   categoryData: Partial<Category>
 ) => {
-  try {
-    const updatedCategory = await prisma.category.update({
-      where: {
-        id: categoryId,
-      },
-      data: categoryData,
-    });
-    return updatedCategory;
-  } catch (error) {
-    throw new Error('Failed to update category'); // Handle error as needed
-  }
+  const updatedCategory = await prisma.category.update({
+    where: {
+      id: categoryId,
+    },
+    data: categoryData,
+  });
+  return updatedCategory;
 };
 
 const deleteCategory = async (categoryId: number) => {
-  try {
-    const deletedCategory = await prisma.category.delete({
-      where: {
-        id: categoryId,
-      },
-    });
-    return deletedCategory;
-  } catch (error) {
-    throw new Error('Failed to delete category'); // Handle error as needed
-  }
+  const deletedCategory = await prisma.category.delete({
+    where: {
+      id: categoryId,
+    },
+  });
+  return deletedCategory;
 };
 
 export const CategoryService = {
