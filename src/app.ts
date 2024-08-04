337ad6b692085469a3c02c9dbc05622bfe7093c1
Rubayet-billah <rubayetbillah909@gmail.com>
2024-08-04 06:39:37 +0600

    set

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,37 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
-
-import cookieParser from 'cookie-parser';
+import { upload } from './config/cloudunaryConfig';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(cookieParser());
-
-//parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', routes);
 
+// Route to handle image uploads
+app.post(
+  '/api/v1/upload',
+  upload.single('image'),
+  (req: Request, res: Response) => {
+    if (req.file) {
+      res.status(httpStatus.OK).json({ url: req.file.path });
+    } else {
+      res.status(httpStatus.BAD_REQUEST).json({ error: 'No file uploaded' });
+    }
+  }
+);
 
-//global error handler
+// Global error handler
 app.use(globalErrorHandler);
 
-//handle not found
+// Handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
